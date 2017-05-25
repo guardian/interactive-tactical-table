8e74284a826b4fcf0d15d536d6a2a36bbca896a7
seanclarkeguardian <sean.clarke@guardian.co.uk>
2017-05-25 11:47:40 +0100

    splitting table out into regions

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 var el = document.createElement('script');
 el.src = '<%= path %>/app.js';
 document.body.appendChild(el);
+
+var parent = window.parent;
+
+var tvstyle = parent.document.createElement('style');
+//tvstyle.type = 'text/css';
+var blob = "h2 em {background-color: blue}; p {background-color: red}";
+
+if (tvstyle.styleSheet){
+  tvstyle.styleSheet.cssText = blob;
+} else {
+  tvstyle.appendChild(document.createTextNode(blob));
+}
+
+parent.document.head.appendChild(tvstyle);
--- src/render.js
+++ src/render.js
@@ -5,11 +5,41 @@ import rawconfig from './config.json!text'
 
 var config = JSON.parse(rawconfig);
 
+
+
+function regionalise(seats) {
+    var regions = [
+        {name: "East", "seats" : []},
+        {name: "East Midlands", "seats" : []},
+ {name: "London", "seats" : []},
+        {name: "North-east", "seats" : []},
+        {name: "North-west", "seats" : []},
+      {name: "  Scotland", "seats" : []},
+        {name: "South-east", "seats" : []},
+        {name: "South-west", "seats" : []},
+   {name: "Wales", "seats" : []},
+        {name: "West Midlands", "seats" : []},
+        {name: "Yorkshire and the Humber", "seats" : []}
+    ];
+    seats.forEach(function (s) {
+        regions.forEach(function (r) {
+            if (r.name == s.location) {
+                    r.seats.push(s);
+            }            
+        })
+    })
+    return regions;
+
+}
+
+
 export async function render() {
     var rawdata = await rp(config.docDataJson);
     var data = JSON.parse(rawdata);
     var seats = data.sheets.Sheet1;
     seats.map(function(s){s.class = s.oppoparty.replace(" ","")})
-    var html = mustache.render(mainTemplate,seats);
+    var regions = regionalise(seats);
+    console.log(regions);
+    var html = mustache.render(mainTemplate,regions);
    return html;
 }
