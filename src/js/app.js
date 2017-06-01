c91e3cf96e32f89c6232509b5b1497dd1ac1cfde
seanclarkeguardian <sean.clarke@guardian.co.uk>
2017-06-01 15:02:37 +0100

    adding in maps

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+
+var mapdivs = document.querySelectorAll('.ge-tv-region-map');
+
+var xhr = new XMLHttpRequest;
+
+xhr.open('GET','<%= path %>/assets/justregionshapes.svg',false);
+xhr.send();
+
+var maphtml = xhr.responseText;
+
+Array.from(mapdivs).forEach(function(d){
+    d.innerHTML = maphtml;
+})
+
+var regions = document.querySelectorAll('path');
+
+Array.from(regions).forEach(function(r){
+    var parentregion = r.parentElement.parentElement.parentElement;
+    console.log(r.id,parentregion.id);
+    if (parentregion.id == r.id) {
+        console.log('match')
+        r.classList.add('gv-tv-selected');
+    }
+})
